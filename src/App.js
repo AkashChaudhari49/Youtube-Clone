4756f88092059df02ea62665bb28161070f7e431
Akash <akshchaudhari49@gmail.com>
2023-01-17 12:51:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Feed from "./components/Feed";
+import Header from "./components/Header";
+import SearchResult from "./components/SearchResult";
+import SearchResultVideoCard from "./components/SearchResultVideoCard";
+import VideoCard from "./components/VideoCard";
+import VideoDetails from "./components/VideoDetails";
+import { AppContext } from "./context/contextApi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Feed />} />
+            <Route path="/searchResult/:searchQuery" element={<SearchResult />} />
+            <Route path="/" element={<SearchResultVideoCard />} />
+            <Route path="/" element={<VideoCard />} />
+            <Route path="/video/:id" element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+
+    </AppContext>
   );
 }
 
